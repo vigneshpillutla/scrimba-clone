7625f934e34a40788eb6fbb392f2b98da8fb4743
Vignesh <vigneshpillutla@gmail.com>
2021-04-13 23:21:57 +0530

    Added responsive header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
 import blob from './svgs/blobDiff.svg';
+import macbookGuy from './svgs/macbook-guy.svg'
+import wheelchair from './svgs/wheelchair.svg'
+import beardedGuy from './svgs/bearded_man.svg'
 import SiteHeader from './SiteHeader';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <SiteHeader/>
-     
     </div>
   );
 }
