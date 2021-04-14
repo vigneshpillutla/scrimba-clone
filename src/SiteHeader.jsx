0dc0f169b9de763d79e7726cf4a16b4b0b4b78eb
Vignesh <vigneshpillutla@gmail.com>
2021-04-14 14:14:12 +0530

    Added responsive font size

--- src/SiteHeader.jsx
+++ src/SiteHeader.jsx
@@ -5,11 +5,13 @@ function SiteHeader(){
     return (
         <div className="site-header-container">
             <div className="site-header">
-                <h1>Don't spend $15,000 on a coding bootcamp</h1>
-                <h3>Our career path helps motivated students become hireable frontend developers for 1% of the cost </h3>
+                <h1 id="var_size_header">Don't spend $15,000 on a coding bootcamp</h1>
+                <h3 id="var_size_text">Our career path helps motivated students become hireable frontend developers for 1% of the cost </h3>
                 <button class="try-out">Try It Out Now!</button>
             </div>
-            <img src={macbookGuy} alt=""/>
+            <div class="standing-guy">
+                <img src={macbookGuy} alt=""/>
+            </div>
         </div>
     )
 }
