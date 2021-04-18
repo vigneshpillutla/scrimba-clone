83c8f462914f892ce8376d09f2790da4feef3283
Vignesh <vigneshpillutla@gmail.com>
2021-04-18 12:08:19 +0530

    added links

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,10 +6,10 @@ function Footer(){
             <div className="media-handles-container">
                 <img src={darkLogo} alt=""/>
                 <div className="link-data">
-                <a href="">Home</a>
-                <a href="">Discord</a>
-                <a href="">Twitter</a>
-                <a href="">Youtube</a>
+                <a href="/">Home</a>
+                <a href="/">Discord</a>
+                <a href="/">Twitter</a>
+                <a href="/">Youtube</a>
                 </div>
 
 
@@ -17,29 +17,29 @@ function Footer(){
             <div className="other-links">
                 <b>Resources</b>
                 <div className="link-data">
-                <a href="">Blog</a>
-                <a href="">FAQ</a>
-                <a href="">Terms</a>
-                <a href="">Privacy</a>
+                <a href="/">Blog</a>
+                <a href="/">FAQ</a>
+                <a href="/">Terms</a>
+                <a href="/">Privacy</a>
                 </div>
             </div>
             <div className="other-links">
                 <b>Courses</b>
                 <div className="link-data">
-                    <a href="">All courses</a>
-                    <a href="">Intro courses</a>
-                    <a href="">Learn Javascript</a>
-                    <a href="">Learn React</a>
+                    <a href="/">All courses</a>
+                    <a href="/">Intro courses</a>
+                    <a href="/">Learn Javascript</a>
+                    <a href="/">Learn React</a>
                 </div>
 
             </div>
             <div className="other-links">
                 <b>Company</b>
                 <div className="link-data">
-                    <a href="">About us</a>
-                    <a href="">Contact us</a>
-                    <a href="">Support chat</a>
-                    <a href="">Gift Membership</a>
+                    <a href="/">About us</a>
+                    <a href="/">Contact us</a>
+                    <a href="/">Support chat</a>
+                    <a href="/">Gift Membership</a>
                 </div>
 
             </div>
